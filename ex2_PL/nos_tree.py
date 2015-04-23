dbe91423380afca5dd3c7d51f96bacbee624ad90
Capibara- <chubi123@gmail.com>
2015-04-23 20:44:32 +0300

    Exercise 2 initial commit.
    Exercise 2 initial commit.

---
+++ ex2_PL/nos_tree.py
@@ -0,0 +1,90 @@
+"""
+Natural Operational Semantics (NOS) of statements, also keeping the derivation tree
+"""
+
+from while_ast import *
+from expr import *
+
+def nos_tree(S, s):
+    """
+    Natural Operational Semantics (NOS) of statements
+
+    Returns (s', tree) such that <S, s> --> s' and tree is the derivation tree
+
+    Implements Table 2.1 from the book.
+
+    --- MODIFY THIS FUNCTION QUESTIONS 1, 3 ---
+    @YAEL: Q1 - DONE
+    """
+
+    if type(S) is Skip:
+        rule = 'skip'
+        premises = ()
+        post_state = s
+
+    elif type(S) is Assign:
+        rule = 'ass'
+        sp = s.copy()
+        sp[S.lhs] = eval_arith_expr(S.rhs, s)
+        premises = ()
+        post_state = sp
+
+    elif type(S) is Comp:
+        rule = 'comp'
+        sp, t1 = nos_tree(S.S1, s)
+        spp, t2 = nos_tree(S.S2, sp)
+        premises = (t1, t2)
+        post_state = spp
+
+    elif type(S) is If and eval_bool_expr(S.b, s) is tt:
+        rule = 'if_tt'
+        sp, t = nos_tree(S.S1, s)
+        premises = (t, )
+        post_state = sp
+
+    elif type(S) is If and eval_bool_expr(S.b, s) is ff:
+        rule = 'if_ff'
+        sp, t = nos_tree(S.S2, s)
+        premises = (t, )
+        post_state = sp
+
+    elif type(S) is While and eval_bool_expr(S.b, s) is tt:
+        rule = 'while_tt'
+        sp, t1 = nos_tree(S.S, s)
+        spp, t2 = nos_tree(While(S.b, S.S), sp)
+        premises = (t1, t2)
+        post_state = spp
+
+    elif type(S) is While and eval_bool_expr(S.b, s) is ff:
+        rule = 'while_ff'
+        premises = ()
+        post_state = s
+
+    else:
+        assert ff  # Error
+
+    return post_state, ('<{}, {}> --> {}\n[{}]'.format(S, s, post_state, rule), premises)
+
+
+if __name__ == '__main__':
+    from tree_to_dot import view_tree
+
+    # @YAEL: factor example given with the code
+    prog = Comp(Assign('y', ALit(1)),
+                While(Not(Eq(Var('x'), ALit(1))),
+                      Comp(Assign('y', Times(Var('y'), Var('x'))),
+                           Assign('x', Minus(Var('x'), ALit(1))))))
+    s, tree = nos_tree(prog, {'x': 5})
+    # @YAEL: GCD from Q1
+    prog = Comp(Assign('t', ALit(0)),
+                While(Not(Eq(Var('b'), ALit(0))),
+                      (Comp(Assign('t', Var('b')),
+                       Assign('b', Comp(Mod(Var('a'), Var('b')), Skip()))),
+                       Comp(Assign('a', Var('t')), Skip()))))
+    s, tree = nos_tree(prog, {'a': 84, 'b': 30})
+    print s
+    print
+    print tree
+    print
+    view_tree(tree)
+
---
+++ ex2_PL/sos.py
@@ -0,0 +1,85 @@
+"""
+Structural Operational Semantics (SOS) of statements
+
+Implemented according to
+http://www.daimi.au.dk/~bra8130/Wiley_book/wiley.pdf (the book).
+
+"""
+
+from while_ast import *
+from expr import *
+
+def sos(S, s):
+    """
+    Structural Operational Semantics (SOS) of statements
+
+    Returns one of the following:
+        s' such that <S, s> ==> s'
+        (S', s') such that <S, s> ==> <S', s'>
+
+    Implements Table 2.2 from the book.
+
+    --- MODIFY THIS FUNCTION QUESTIONS 1, 3 ---
+    @YAEL: Q1 - DONE
+    """
+
+    if type(S) is Skip:
+        return s
+
+    elif type(S) is Assign:
+        sp = s.copy()
+        sp[S.lhs] = eval_arith_expr(S.rhs, s)
+        return sp
+
+    elif type(S) is Comp:
+        gamma = sos(S.S1, s)
+        if type(gamma) is tuple:
+            S1p, sp = gamma
+            return (Comp(S1p, S.S2), sp)
+        else:
+            sp = gamma
+            return (S.S2, sp)
+
+    elif type(S) is If and eval_bool_expr(S.b, s) is tt:
+        return (S.S1, s)
+
+    elif type(S) is If and eval_bool_expr(S.b, s) is ff:
+        return (S.S2, s)
+
+    elif type(S) is While:
+        return (If(S.b, Comp(S.S, While(S.b, S.S)), Skip()), s)
+
+    else:
+        assert ff  # Error
+
+
+def run_sos(S, s):
+    """
+    Iteratively apply sos until a final state is reached.
+    """
+
+    gamma = (S, s)
+    while type(gamma) is tuple:
+        S, s = gamma
+        print '<{}, {}> ==>\n'.format(S, s)
+        gamma = sos(S, s)
+    print gamma
+    return gamma
+
+
+if __name__ == '__main__':
+    # @YAEL: factoring example. given with the code
+    """
+    prog = Comp(Assign('y', ALit(1)),
+                While(Not(Eq(Var('x'), ALit(1))),
+                      Comp(Assign('y', Times(Var('y'), Var('x'))),
+                           Assign('x', Minus(Var('x'), ALit(1))))))
+    run_sos(prog, {'x': 5})
+    """
+    # @YAEL: GCD from Q1
+    prog = Comp(Assign('t', ALit(0)),
+                While(Not(Eq(Var('b'), ALit(0))),
+                      (Comp(Assign('t', Var('b')),
+                       Assign('b', Comp(Mod(Var('a'), Var('b')), Skip()))),
+                       Comp(Assign('a', Var('t')), Skip()))))
+    run_sos(prog, {'a': 84, 'b': 30})
